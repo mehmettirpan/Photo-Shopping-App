4bb53d2e8227e0e04eb4d0650f39b5ed05eed8d1
mehmettirpan <mehmettirpan92@gmail.com>
2024-07-19 13:50:06 +0300

    Login Screen Update

--- tsoftInternshipProject/ui/View/LoginViewController.swift
+++ tsoftInternshipProject/ui/View/LoginViewController.swift
@@ -90,6 +90,12 @@ class LoginViewController: UIViewController {
             designedByLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
             designedByLabel.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
         ])
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
     }
 
     @objc func loginButtonTapped() {
@@ -166,5 +172,20 @@ class LoginViewController: UIViewController {
 
             self.present(tabBarController, animated: true, completion: nil)
         }
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            self.view.frame.origin.y = -keyboardHeight / 2
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        self.view.frame.origin.y = 0
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
 }
